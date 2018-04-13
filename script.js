cf24f609d6bfe09b4c0d4fe762cab2afbd0347c6
V.Glushakov <vaoooglu.dn@gmail.com>
2018-04-13 22:00:03 +0300

    third_13.04.18

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ console.log(newUser);
   newCounter.decrease();
   newCounter.showRes();
 */
-  var Robot=
+  /*var Robot=
   function(x,y) {
     this.x=0;
     this.y=0;
@@ -48,7 +48,7 @@ console.log(newUser);
           case this.y>10||this.y<-10:
             alert("STOP y is more then 10")
             break;
-      //    default:continue;
+
         }
     };
     Robot.prototype.left= function(){
@@ -57,7 +57,7 @@ console.log(newUser);
           case this.y>10||this.y<-10:
             alert("STOP y is more then 10")
             break;
-        //  default:continue;
+
         }
     };
     Robot.prototype.back= function(){
@@ -66,7 +66,7 @@ console.log(newUser);
           case this.x>10||this.x<-10:
             alert("STOP x is more then 10")
             break;
-      //    default:continue;
+
         }
     };
     Robot.prototype.forward= function(){
@@ -75,8 +75,7 @@ console.log(newUser);
           case this.x>10||this.x<-10:
             alert("STOP x is more then 10")
             break;
-      //    default:continue;
-        }
+      }
     };
     Robot.prototype.showRes= function(){
     //  switch (true) {
@@ -116,3 +115,33 @@ console.log(newUser);
   newRobot.back();
   newRobot.back();
   newRobot.showRes();
+*/
+var MyQuery = function(selector){
+  this.elem=document.querySelector(selector);
+  var hlcolor="yellow";
+  this.getHlcolor=function (){
+    return hlcolor;
+  }
+    this.setHlcolor=function(color){
+      if (["red", "green", "yellow"].indexOf(color) >= 0) {
+        hlcolor=color;
+      }else {
+        console.error("Choose another color")
+      }
+    }
+
+};
+MyQuery.prototype.setHeight=function (n) {
+  this.elem.style.height=n+"px";
+};
+MyQuery.prototype.heightLight=function () {
+  this.elem.style.background=this.getHlcolor();
+}
+var myElem=new MyQuery("#robot_status");
+myElem.setHeight("200");
+myElem.hlcolor="black";
+myElem.heightLight();
+var myElem1=new MyQuery(".second");
+myElem1.setHeight("150");
+myElem1.setHlcolor("green");
+myElem1.heightLight();
